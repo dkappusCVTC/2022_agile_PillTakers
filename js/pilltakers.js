6fcf078d3d7a14c60dde1bb25338ffe1868dac97
cheesekat <112413246+cheesekat@users.noreply.github.com>
2022-10-10 09:58:13 -0500

    update choose profile icon

--- js/pilltakers.js
+++ js/pilltakers.js
@@ -1,11 +1,12 @@
 /* jshint curly: true, esversion: 6, eqeqeq: true, latedef: true, laxbreak: true */
 // account.html JS Code Start
-function newUser(uname, pwd, fname, lname, e_mail) {
+function newUser(uname, pwd, fname, lname, e_mail, icon) {
     this.uname = uname;
     this.pwd = pwd;
     this.fname = fname;
     this.lname = lname;
     this.e_mail = e_mail;
+    this.icon = icon;
 }
 
 var acctNumber = 0;
@@ -19,7 +20,9 @@ function createAccount() {
     var userLastName = document.getElementById("lastnametext").value;
     var message = '';
     
-    accounts[acctNumber] = new newUser(username, password, userFirstName, userLastName, userEmail);
+    changeIcon();
+    
+    accounts[acctNumber] = new newUser(username, password, userFirstName, userLastName, userEmail, mainIcon);
     acctNumber++;
     localStorage.setItem("accountNumber", accounts);
 
@@ -28,6 +31,43 @@ function createAccount() {
     document.getElementById("message").innerHTML = message;
 
 } // end createAccount()
+// choose profile icon
+var mainIcon = document.getElementById("mainicon");
+  var icons = document.getElementById("icons");
+  var value = icons.value;
+  var text = icons.options[icons.selectedIndex].text;
+
+// fix this
+function changeIcon() {
+  if (icons.options[icons.selectedIndex].text == "007") {
+    mainIcon.src = "/images/007.jpg";
+  } else if (icons.options[icons.selectedIndex].text == "Afro") {
+    mainIcon.src = "/images/Afro.png";
+  } else if (icons.options[icons.selectedIndex].text == "Clown") {
+    mainIcon.src = "/images/Clown.png";
+  } else if (icons.options[icons.selectedIndex].text == "Cowboy") {
+    mainIcon.src = "/images/Cowboy.png";
+  } else if (icons.options[icons.selectedIndex].text == "Doctor") {
+    mainIcon.src = "/images/Doctor.png";
+  } else if (icons.options[icons.selectedIndex].text == "Elvis") {
+    mainIcon.src = "/images/Elvis.png";
+  } else if (icons.options[icons.selectedIndex].text == "FatherXmas") {
+    mainIcon.src = "/images/FatherXmas.png";
+  } else if (icons.options[icons.selectedIndex].text == "Ghost") {
+    mainIcon.src = "/images/Ghost.png";
+  } else if (icons.options[icons.selectedIndex].text == "Pirate") {
+    mainIcon.src = "/images/Pirate.png";
+  } else if (icons.options[icons.selectedIndex].text == "Policeman") {
+    mainIcon.src = "/images/Policeman.png";
+  } else if (icons.options[icons.selectedIndex].text == "Superhero") {
+    mainIcon.src = "/images/Superhero.png";
+  } else if (icons.options[icons.selectedIndex].text == "TeddyBear") {
+    mainIcon.src = "/images/TeddyBear.png";
+  } else {
+    mainIcon.src = "/images/add-user.png";
+  }
+}
+
 // account.html JS Code End
 
 // medications.html JS Code Start
